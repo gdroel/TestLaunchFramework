be5a80dfa5f9cbde569cc2a685a3d65a12b8a721
Gabe Roeloffs <gdroel@gmail.com>
2021-03-30 11:10:58 -0700

    Initial Commit

---
+++ TestLaunchFramework/TestLaunchFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestLaunchFramework.h
+//  TestLaunchFramework
+//
+//  Created by Gabe Roeloffs on 3/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLaunchFramework.
+FOUNDATION_EXPORT double TestLaunchFrameworkVersionNumber;
+
+//! Project version string for TestLaunchFramework.
+FOUNDATION_EXPORT const unsigned char TestLaunchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLaunchFramework/PublicHeader.h>
+
+
